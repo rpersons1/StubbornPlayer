57c2b9f2cfcdec7a4b18511d4ee696367ab0d678
rpersons1 <rpersons1@mail.csuchico.edu>
2018-03-01 12:22:21 -0800

    made a program that plays a game

--- main.cpp
+++ main.cpp
@@ -6,6 +6,24 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int repe;
+  int wins;
+  
+  cout<<"How many times do you want to play the game? ";
+  cin>>repe;
+
+  for(int x = 0; x < repe; x++)
+  {
+    game.newGame();
+    game.guessDoorC();
+    game.guessDoorC();
+    if(game.isWinner() == true)
+    {
+      wins++;
+    }
+  }
+  cout<<wins<<endl;
+
+
   return 0;
 }
